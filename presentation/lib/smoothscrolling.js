b62c8757b8e4a51c8ef2513f7675d2a834d37ef0
Lenard Wiedekamp <lenwdk@gmail.com>
2016-07-01 01:34:13 +0200

    smoothscrolling, needs some improvement

---
+++ presentation/lib/smoothscrolling.js
@@ -0,0 +1,34 @@
+// http://stackoverflow.com/questions/17722497/scroll-smoothly-to-specific-element-on-page
+
+function smoothScroll(eID) {
+    var startY = 0;
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+
+
+    speed =18;
+    var step = Math.round(distance / 5000);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+    return false;
+}
+
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
