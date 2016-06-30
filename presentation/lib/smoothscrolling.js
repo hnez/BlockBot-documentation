acbe942e492d37e5a7f86a761aaf198e8e5e48b1
Lenard Wiedekamp <lenwdk@gmail.com>
2016-07-01 01:57:38 +0200

    Im happy with the scrolling now

--- presentation/lib/smoothscrolling.js
+++ presentation/lib/smoothscrolling.js
@@ -1,34 +1,35 @@
 // http://stackoverflow.com/questions/17722497/scroll-smoothly-to-specific-element-on-page
 
+/* The code is simplified for this application */
+
 function smoothScroll(eID) {
     var startY = 0;
-    var stopY = elmYPosition(eID);
-    var distance = stopY > startY ? stopY - startY : startY - stopY;
 
-
-    speed =18;
-    var step = Math.round(distance / 5000);
-    var leapY = stopY > startY ? startY + step : startY - step;
-    var timer = 0;
-    if (stopY > startY) {
-        for ( var i=startY; i<stopY; i+=step ) {
-            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-        } return;
-    }
-    for ( var i=startY; i>stopY; i-=step ) {
-        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-    }
-    return false;
-}
-
-function elmYPosition(eID) {
     var elm = document.getElementById(eID);
-    var y = elm.offsetTop;
+    var stopY = elm.offsetTop;
     var node = elm;
     while (node.offsetParent && node.offsetParent != document.body) {
         node = node.offsetParent;
-        y += node.offsetTop;
-    } return y;
+        stopY += node.offsetTop;
+    }
+
+    var distance = stopY - startY;
+    var step = Math.round(distance / 5000);
+    var leapY = startY + step;
+
+
+    setInterval(function(){
+
+      window.scrollTo(0, leapY);
+
+      if(leapY==stopY){
+        //reset
+        leapY = startY;
+      }
+
+      leapY += step;
+      if (leapY > stopY) {
+        leapY = stopY;
+      }
+    }, 20);
 }
